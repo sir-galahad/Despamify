2305ee15369b413417e9ac7cb2ed8be60100394f
Aaron Davis <AaronD_01@yahoo.com>
2022-01-22 06:41:44 -0500

    filtering based on time, need to prune arrays next

--- Despamify.lua
+++ Despamify.lua
@@ -1,12 +1,12 @@
 local hooks = {}
 local lastlines = {} 
 local toggle = 2 -- 2 = on 1 = off
-
 local MessageClass = {}
-
+local timeout = 10 -- seconds to filter posts
 function MessageClass:new(obj)
 	setmetatable(obj,self)
 	self.__index = self
+	return obj
 end
 	
 local function AddMessage(self, message, ...)
@@ -15,20 +15,39 @@ local function AddMessage(self, message, ...)
 	
 	-- message includes what appears to be a timestamp 
 	-- in the form of seconds since login, substitute it out to compare
+	local tmp = string.gsub(message,':[0-9]+:',"xxx")
+	
+	-- this bit with the two matches is pretty gross
+	local timestamp = string.match(message, ':([0-9]+):')
+	
+	if ( timestamp == nil) then
+		return hooks[self](self, message, ...)
+	end
 
-	tmp = string.gsub(message,':[0-9]+:',"xxx")
-	timestamp = string.match(message, "(:[0-9]+:)")
-	MessageClass:new{message=tmp, timestamp=timestamp}
+	msg = MessageClass:new{message=tmp, timestamp=tonumber(timestamp)}
+
 	-- if turned off do no filtering
 	if(toggle == 1) then
 		lastlines[self] = tmp 
-		return hooks[self](self, message, ...)
+		return hooks[self](self, "("..channel..'+'..timestamp..")"..message, ...)
 	end
 	
-	-- don't let the same line through twice
-	if(lastlines[self] ~= tmp) then
-		lastlines[self] = tmp
-		return hooks[self](self, message, ...)
+	
+	if(lastlines[tmp] == nil or 
+			lastlines[tmp]["timestamp"] < msg["timestamp"] - timeout) then
+		oldmsg = lastlines[tmp]
+
+		local oldtimeout
+		if(oldmsg ~= nil) then
+			oldtimeout = lastlines[tmp]["timestamp"]
+		else
+			oldtimeout = "nil"
+		end
+
+		lastlines[tmp] = msg
+		
+		--print(lastlines[tmp].message)
+		return hooks[self](self, tmp.."("..msg.timestamp.."vs"..oldtimeout..")", ...)
 	end
 
 	return ""
